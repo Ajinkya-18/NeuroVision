9f1eec8fddb61c45e1d99cc1923bace74f6ff834
Ajinkya Tamhankar <ajinkya.tamhankar18@gmail.com>
2025-08-27 01:06:40 +0530

    Periodic commit.

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -9,7 +9,7 @@ import multiprocessing as mp
 if __name__ == "__main__":
     mp.freeze_support()
 
-    root_dir = Path('../data/Class_Combined_Balanced_Dataset')
+    root_dir = Path('../NeuroVision/data/Class_Combined_Balanced_Dataset')
 
     train_dataset, val_dataset = make_datasets(root_dir=root_dir)
 
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                             num_workers=4, pin_memory=False, persistent_workers=True, prefetch_factor=2)
 
 
-    lstm_model = EegLstm(input_dims=5, hidden_dims=128, num_layers=3, dropout=0.3, num_classes=len(os.listdir(root_dir)))
+    lstm_model = EegLstm(input_dims=5, hidden_dims=128, num_layers=2, dropout=0.3, num_classes=len(os.listdir(root_dir)))
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
